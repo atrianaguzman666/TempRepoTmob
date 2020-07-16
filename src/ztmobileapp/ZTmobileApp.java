04eb65a8dfaedcf67667497dcc9b8be8fbd4e046
Jose Alfonso <atrianaguzman@hotmail.com>
2020-07-16 13:54:29 -0400

    Second Change

--- src/ztmobileapp/ZTmobileApp.java
+++ src/ztmobileapp/ZTmobileApp.java
@@ -15,7 +15,7 @@ public class ZTmobileApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HI HI HI");
     }
     
 }
